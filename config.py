10813317597ff628bdc42a9792ff43e581bc9b03
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-19 00:05:02 -0800

    config python file complete

---
+++ config.py
@@ -0,0 +1,5 @@
+GROUP_SIZES = range(2, 101)
+TRIALS = 10000
+OUTPUT_SIMULATION_FILE = "outputs/simulation_results.csv"
+OUTPUT_ANALYSIS_FILE = "outputs/analysis_results.csv"
+VISUALIZATIONS_FOLDER = "outputs/visualizations"
